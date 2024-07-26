983257c5bb83ea2d574b72c33b5ec359ebd6be55
cyomega <106396564+cyomega@users.noreply.github.com>
2024-07-26 16:54:07 +0800

    Update items.js

--- items.js
+++ items.js
@@ -5,10 +5,14 @@ $(document).ready(function() {
 	const table = $('#tableArray').DataTable({
 		columns: [
 			{title: '中文'},
-			{title: '英文'}
+			{title: '英文（雙擊複製）'}
 		],
 		fixedColumns: true,
 		search: {regex: true},
 		pageLength: 25
+	});
+	$('#tableArray').on('dblclick', 'tr', function () {
+		let data = table.row(this).data();
+		navigator.clipboard.writeText(data[1]);
 	});
 });
